60e583597249d0e00e30774fdd48175743de4866
aalokbuilds <aaloksingh9953@gmail.com>
2026-09-24 09:54:55 +0530

    Add student feedback application and CI/CD pipeline

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+feedback_list = []
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        course = request.form["course"]
+        feedback = request.form["feedback"]
+
+        feedback_list.append({
+            "name": name,
+            "course": course,
+            "feedback": feedback
+        })
+
+    return render_template("index.html", feedbacks=feedback_list)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
